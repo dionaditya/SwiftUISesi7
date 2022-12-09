1e408391f73ae26f9fb4535fcc6c173cd3ad0260
Dion Aditya <dion.aditya@kiteworks.com>
2022-12-09 21:01:57 +0700

    Initial Commit

---
+++ SwiftUISesi7/SwiftUISesi7App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUISesi7App.swift
+//  SwiftUISesi7
+//
+//  Created by Dion Aditya on 9/12/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISesi7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
